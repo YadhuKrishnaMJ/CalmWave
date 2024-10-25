d02570352cb61847a1ccf4d18bcc124ba4fe977f
Yadhu Krishna M J <yadddhu@gmail.com>
2024-10-25 11:25:48 +0530

    New Features

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_from_directory
 import pickle
 import numpy as np
 import librosa
@@ -7,7 +7,9 @@ import os
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
+BINAURAL_FOLDER = 'binauralwaves'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['BINAURAL_FOLDER'] = BINAURAL_FOLDER
 
 # Load the model
 with open('emotion_recognition_model.pkl', 'rb') as file:
@@ -26,6 +28,18 @@ emotions = {
 }
 
 observed_emotions = ['calm', 'happy', 'fearful', 'disgust']
+
+# Map emotions to corresponding audio files
+emotion_to_audio = {
+    'calm': 'alpha.mp3',
+    'happy': 'beta.mp3',
+    'fearful': 'theta.mp3',
+    'disgust':  'theta.mp3',
+    'sad':  'alpha.mp3',
+    'surprised':  'beta.mp3',
+    'neutral':  'beta.mp3',
+    'disgust':  'alpha.mp3'
+}
 
 def extract_feature(file_name, mfcc=True, chroma=True, mel=True):
     with sf.SoundFile(file_name) as sound_file:
@@ -66,11 +80,22 @@ def predict():
         prediction = model.predict(feature)
         emotion = prediction[0]
 
-        return jsonify({'emotion': emotion})
+        if emotion in emotion_to_audio:
+            audio_file = emotion_to_audio[emotion]
+            audio_path = os.path.join(app.config['BINAURAL_FOLDER'], audio_file)
+            return jsonify({'emotion': emotion, 'audio': audio_file})
+        else:
+            return jsonify({'emotion': emotion, 'audio': None})
 
     return jsonify({'error': 'Invalid file format'}), 400
+
+@app.route('/audio/<filename>')
+def send_audio(filename):
+    return send_from_directory(app.config['BINAURAL_FOLDER'], filename)
 
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
+    if not os.path.exists(BINAURAL_FOLDER):
+        os.makedirs(BINAURAL_FOLDER)
     app.run(debug=True)
